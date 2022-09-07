93de46f74bd08a0225adb71d5a4c7a6e156b897a
Gabor Molnar <gabor_molnar@epam.com>
2022-09-07 23:10:38 +0200

    log messages for debugging

--- index.js
+++ index.js
@@ -20,23 +20,31 @@ client.on("ready", () => {
         channelIdsArr.push(channelId)
     });
 
+    console.log(`langs=${langsArr}`)
+    console.log(`channels=${channelIdsArr}`)
+
     console.log("Translator Bot is connected to Discord.")
 });
 
 client.on("message", async (message) => {
 
     const messageChannelId = message.channel.id
+    console.log(`messageChannelId=${messageChannelId}`);
 
     if (channelIdsArr.includes(messageChannelId)) {
         const messageChannel = client.channels.cache.get(messageChannelId);
+        console.log(`messageChannel=${messageChannel}`);
         if(messageChannel) {
             const messageLang = langsArr[channelIdsArr.indexOf(messageChannelId)]
+            console.log(`messageLang=${messageLang}`);
 
             const translateToChannels = channelIdsArr.filter(c => c !== messageChannelId)
             for (let i = 0 ; i < translateToChannels.length ; i++) {
                 const langIndex = channelIdsArr.indexOf(translateToChannels[i])
                 const channel = client.channels.cache.get(translateToChannels[i])
                 const toLang = langsArr[langIndex]
+                console.log(`channel=${channel}`);
+                console.log(`toLang=${toLang}`);
 
                 const translation = await Translator.autoTranslate(message, { from: messageLang, to: toLang});
                 if(translation) return channel.send(translation)
